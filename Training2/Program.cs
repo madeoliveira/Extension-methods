d496eb5d3025a33769a1a7d42dea820128ed27fd
Marcio Oliveira <marcio.alves78@hotmail.com>
2021-02-12 16:56:45 -0300

    receive an integer value as a parameter and generate a clipping of the original string.

---
+++ Training2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
