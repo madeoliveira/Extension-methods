b7689cb31f77d17317607f048cdfc61f13b3cf34
Marcio Oliveira <marcio.alves78@hotmail.com>
2021-02-12 16:30:59 -0300

    Displays a DateTime object in the form of time elapsed, which can be in hours or days.

---
+++ Training/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 02, 12, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
